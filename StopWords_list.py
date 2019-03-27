a3a690e3135cf7630fcd27cc78074c083a86b8e8
Devasish Mahato <t7.devasishmahato@gmail.com>
2019-03-27 10:11:45 +0530

    Update StopWords_list.py

--- StopWords_list.py
+++ StopWords_list.py
@@ -1,1 +1,2 @@
+# The file contains few stop words which are neccesary in order to find similarity between abbreviations and their full forms.
 StopList=['and','with','is','of','for','of','in','on','&']
