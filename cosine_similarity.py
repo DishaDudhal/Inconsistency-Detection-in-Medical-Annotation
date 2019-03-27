a938b40943b091eafd39a65d4b471e329c2bbcc8
Devasish Mahato <t7.devasishmahato@gmail.com>
2019-03-27 10:17:48 +0530

    Update cosine_similarity.py

--- cosine_similarity.py
+++ cosine_similarity.py
@@ -1,11 +1,15 @@
 # The function cosine similarity takes two arguments (a,b) where a and b bore a word or word phrases.
+
 def cosine(a,b):
+    
     #creating a set of words from the word phrase
     set1=set(a.split())
     set2=set(b.split())
+    
     # Finding the dissimilar words in both the sets
     diff_LR=list(set1-set2)
     diff_RL=list(set2-set1)
+    
     # Calculating Similarity Score
     similar_score=(len(set1)-len(diff_LR))/len(set1)
     return(similar_score)
